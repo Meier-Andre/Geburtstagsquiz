07e8932dcc004a3ac1d9b3fc14e66d8d54a8ff6a
Andre Meier <andre_meier_@t-online.de>
2016-11-03 15:06:15 +0100

    .

--- createUser.php
+++ createUser.php
@@ -1,54 +1,85 @@
+
 <?php
 
-//header( 'Location: http://192.168.178.40/quiz/index.php?user='.$_POST["name"].'');
-$name = $_POST["name"];
-
-                $server = "localhost";
-                $user = "allinone";
-                $pw = "cAf8HBs5EqvKppec";
-                $db = "allinone";
-
-                $DB_ID = mysql_connect($server, $user, $pw);
-                if($DB_ID == false) {
-                    echo "<option>Öffnen der DB fehlgeschlagen</option>";
-                } else {
-                    mysql_query("use $db", $DB_ID);
-                    mysql_query('set names utf8');
-                    $erg = mysql_query("select ID, NAME, KATEGORY from places WHERE KATEGORY = '' order by ID ;");
-                    $anzahl = mysql_num_rows($erg);
-                    echo $anzahl;
-                }
-/*
+//
+$name = $_GET["name"];
 $server = "localhost";
 $user = "allinone";
 $pw = "cAf8HBs5EqvKppec";
-$db = "Quiz";
+$db = "quiz";
 
-
-$DB_ID1 = mysql_connect($server, $user, $pw);
-if($DB_ID1 == false) {
-echo "Öffnen der DB fehlgeschlagen<br>";
+$DB_ID = mysql_connect($server, $user, $pw);
+if($DB_ID == false) {
+    echo "<option>Öffnen der DB fehlgeschlagen</option>";
 } else {
-    mysql_query("use $db", $DB_ID1);
+    mysql_query("use $db", $DB_ID);
     mysql_query('set names utf8');
-    $erg = mysql_query("select ID, Name from User WHERE Name = $name");
+    $erg = mysql_query("select ID, Name from User WHERE Name = '$name' order by ID;");
     $anzahl = mysql_num_rows($erg);
-    if ($anzahl > 0) {
-        echo "User bereits vorhanden";
+    if ($anzahl >= 1) {
+        
+        ?>
+        <html>
+        <head>
+            <title>Quiz</title>
+            <style>
+            h1, a {
+                width: 100%;
+                text-align: center;
+                font: 50px Arial,sans-serif;
+            }
+            a {
+                color: #A1B9C5;
+            }
+            input[type=submit] {
+                /*cursor: pointer;*/
+                font: 50px Arial,sans-serif;
+                color: #FFFFFF; 
+                background-color: #A1B9C5;
+                height: 100%;
+                width: 100%;
+                /*padding: 3px;
+                line-height: 100%%;*/
+                -moz-border-radius: 0px !important;  /* for mozilla */
+                -webkit-border-radius: 0px !important; /* for safari */
+                border-radius: 0;
+                -webkit-appearance: none;
+            }
+            </style>
+        </head>
+        <body>
+            <table width=100% height=100%>
+                <tr>
+                    <td colspan="2" align="center" height=10%><h1>Benutzername bereits vergeben</h1></td>
+                </tr>
+                <tr>
+                    <td align="center" width="50%"><A HREF="index.php">zurück</A></td>
+                    <td align="center" width="50%">
+                        <A HREF="<?php echo 'index.php?user='.$_GET["name"].''; ?>">Ich habe mich<br> bereits mit diesem<Br> Benutzernamen registriert</A>
+                    </td>
+                </tr>
+            </table>
+        <?php
+            
+        
+        ?>
+            </body></html><?php
+    } else {
+        header( 'Location: http://192.168.178.40/quiz/index.php?user='.$_GET["name"].'');
+        $DB_ID = mysql_connect($server, $user, $pw);
+        if($DB_ID == false) {
+        echo "Öffnen der DB fehlgeschlagen<br>";
+        } else {
+            mysql_query("use $db", $DB_ID);
+            mysql_query('set names utf8');
+            mysql_query("insert into User (NAME) values ('$name')");
+            if(mysql_errno()) {
+                echo mysql_error();
+            } else {
+                //echo "Die daten wurden erfolgreich eingetragen";
+            }
+        }
     }
 }
 
-$DB_ID = mysql_connect($server, $user, $pw);
-    if($DB_ID == false) {
-    echo "Öffnen der DB fehlgeschlagen<br>";
-    } else {
-        mysql_query("use $db", $DB_ID);
-        mysql_query('set names utf8');
-        mysql_query("insert into User (NAME) values ('$name')");
-        if(mysql_errno()) {
-            echo mysql_error();
-        } else {
-            //echo "Die daten wurden erfolgreich eingetragen";
-        }
-    }*/
 ?>
--- index.php
+++ index.php
@@ -35,6 +35,24 @@ if(isset($_GET['user']))
     $user = "allinone";
     $pw = "cAf8HBs5EqvKppec";
     $db = "Quiz";
+    
+    $u_id = "0";
+    $DB_ID1 = mysql_connect($server, $user, $pw);
+    if($DB_ID1 == false) {
+    echo "Öffnen der DB fehlgeschlagen<br>";
+    } else {
+        mysql_query("use $db", $DB_ID1);
+        mysql_query('set names utf8');
+        $username = $_GET['user'];
+        $erg = mysql_query("select ID, Name from User WHERE Name='$username';");
+        $anzahl = mysql_num_rows($erg);
+        if ($anzahl == 1) {
+            for ($i=0; $i < $anzahl; $i++) {
+                $zeile = mysql_fetch_row($erg);
+                $u_id = $zeile[0];
+            }
+        }
+    }
 
     $DB_ID = mysql_connect($server, $user, $pw);
     if($DB_ID == false) {
@@ -45,32 +63,58 @@ if(isset($_GET['user']))
         $erg = mysql_query("select ID, FRAGE, A, B, C, D, Antwort, Active from Fragen WHERE active='1';");
         $anzahl = mysql_num_rows($erg);
         if ($anzahl == 1) {
-            for ($i=0; $i < $anzahl; $i++) {
-
-                $zeile = mysql_fetch_row($erg);
-                ?>
-                <form action='save.php' method='POST'>
-                    <table width=100% height=100%>
-                        <tr>
-                            <td colspan="2" align="center" height=10%><h1><?php echo $zeile[1]; ?></h1></td>
-                        </tr>
-                        <tr width=50%>
-                            <td align="center" width=50%><input style="border: none" type='submit' name='A' value='<?php echo $zeile[2]; ?>'/></td>
-                            <td align="center" width=50%><input style="border: none" type='submit' name='B' value='<?php echo $zeile[3]; ?>'/></td>
-                        </tr>
-                        <tr>
-                            <td align="center"><input style="border: none" type='submit' name='C' value='<?php echo $zeile[4]; ?>'/></td>
-                            <td align="center"><input style="border: none" type='submit' name='D' value='<?php echo $zeile[5]; ?>'/></td>
-                        </tr>
-                        <input hidden="true" type='text' name='name' value='<?php echo $_GET['user']; ?>'>
-                        <input hidden="true" type='text' name='frage' value='<?php echo $zeile[0]; ?>'>
-                        <tr>
-                            <td colspan="2" align="center" height=10%><A HREF="javascript:history.go(0)">Aktualisieren</A></td>
-                        </tr>
-                    </table>
-                </form>
-                <?php
+            $zeile = mysql_fetch_row($erg);
+            $f_id = $zeile[0];
+            $DB_ID2 = mysql_connect($server, $user, $pw);
+            if($DB_ID2 == false) {
+            echo "Öffnen der DB fehlgeschlagen<br>";
+            } else {
+                mysql_query("use $db", $DB_ID2);
+                mysql_query('set names utf8');
+                $e = mysql_query("select ID, F_ID, U_ID from Antworten WHERE F_ID='$f_id' AND U_ID ='$u_id';");
+                $a = mysql_num_rows($e);
+                if ($a > 0) {
+                    ?>
+                        <table width=100% height=100%>
+                            <tr>
+                                <td colspan="2" align="center" height=10%><h1><?php echo $zeile[1]; ?></h1></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2" align="center" height=10%><h1>Die Anwort zu dieser Frage wurde bereits abgegeben</h1></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2" align="center" height=10%><A HREF="javascript:history.go(0)">Aktualisieren</A></td>
+                            </tr>
+                        </table>
+                    <?php
+                } else {
+                    for ($i=0; $i < $anzahl; $i++) {
+                        ?>
+                        <form action='save.php' method='POST'>
+                            <table width=100% height=100%>
+                                <tr>
+                                    <td colspan="2" align="center" height=10%><h1><?php echo $zeile[1]; ?></h1></td>
+                                </tr>
+                                <tr width=50%>
+                                    <td align="center" width=50%><input style="border: none" type='submit' name='A' value='<?php echo $zeile[2]; ?>'/></td>
+                                    <td align="center" width=50%><input style="border: none" type='submit' name='B' value='<?php echo $zeile[3]; ?>'/></td>
+                                </tr>
+                                <tr>
+                                    <td align="center"><input style="border: none" type='submit' name='C' value='<?php echo $zeile[4]; ?>'/></td>
+                                    <td align="center"><input style="border: none" type='submit' name='D' value='<?php echo $zeile[5]; ?>'/></td>
+                                </tr>
+                                <input hidden="true" type='text' name='name' value='<?php echo $_GET['user']; ?>'>
+                                <input hidden="true" type='text' name='frage' value='<?php echo $zeile[0]; ?>'>
+                                <tr>
+                                    <td colspan="2" align="center" height=10%><A HREF="javascript:history.go(0)">Aktualisieren</A></td>
+                                </tr>
+                            </table>
+                        </form>
+                        <?php
+                    }
+                }
             }
+            
         } else {
         ?>
         <table width=100% height=100%>
@@ -86,7 +130,7 @@ if(isset($_GET['user']))
     }
 } else {
     ?>
-    <form action='createUser.php' method='POST'>
+    <form action='createUser.php' method='GET'>
         <table width=100%>
             <tr>
                 <td align="center">Benutzernamen eingeben:</td>
